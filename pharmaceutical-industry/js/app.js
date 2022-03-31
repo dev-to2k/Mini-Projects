c01388f868428fa77aaca0743eed7fb612b42150
dev-to2k <72263236+dev-to2k@users.noreply.github.com>
2022-03-31 15:30:13 +0700

    commit

---
+++ pharmaceutical-industry/js/app.js
@@ -0,0 +1,7 @@
+const btnSearch = document.getElementById("btn-search");
+const inputSearch = document.getElementById("input-search");
+
+// Toggle click btnSearch
+btnSearch.addEventListener("click", function () {
+  inputSearch.classList.toggle("show");
+});
